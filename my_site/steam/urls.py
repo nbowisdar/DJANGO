e4dcab9218356905f8a04f209df5631449fd7292
nbowisdar <78906490+nbowisdar@users.noreply.github.com>
2022-02-05 12:14:36 +0200

    one

--- my_site/steam/urls.py
+++ my_site/steam/urls.py
@@ -3,5 +3,6 @@ from steam.views import *
 
 urlpatterns = [
     path('test/', funk),
-    path('', pattern)
+    path('', pattern),
+    path('test_temp', test_template)
 ]
--- my_site/steam/views.py
+++ my_site/steam/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import Template, Context
+import my_site.settings as x
+
+
+context = {'name':'Mari', 'age':'19'}
 
 def funk(requests):
-    return HttpResponse('I get this')
+    return HttpResponse(f'<h1>{x.BASE_DIR}</h1>')
+
+def test_template(requests):
+    # d = Template(text)
+    # rez = d.render(context)
+    # return HttpResponse(rez)
+    return render(requests, 'index.html', context)
 
 def pattern(requests):
-    return render(requests, 'index.html')
+    return render(requests, 'steam/login_page.html')
